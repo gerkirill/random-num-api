ff2963dcc0cd9c14b6280040f7f2cf6dedf6de38
kirill <ger.kirill@gmail.com>
2018-04-26 18:05:06 +0300

    adds proper content-type header for the json response and automatically redirect from root URL to /randomnum

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var cors = require('cors');
 
 var app = express();
 app.use(cors());
+app.get('/', (req, res) => res.redirect('/randomnum'));
 
 // >>> НАЧАЛО КОДА СЕРВЕРА
 app.get('/randomnum', function (req, res) {
   const randomNum = Math.floor( Math.random() * 10 );
+  res.set('Content-Type', 'application/json');
   res.send({ message: randomNum });
 });
 // >>> КОНЕЦ КОДА СЕРВЕРА
